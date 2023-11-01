e9bdd67aededf42bcfb62b28dfacff0d839dfedf
Ivan <ivanvasyunin@gmail.com>
2023-11-01 18:55:18 +0100

    remaining logic added

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -9,25 +9,34 @@ const TimerChallenge = ({ title, targetTime }) => {
 
   const isTimerActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-  if(timeRemaining <= 0) {
+  if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
     dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
 
   function handleStart() {
     timer.current = setInterval(() => {
-      setTimeRemaining( prevTimeRemaining => prevTimeRemaining - 10);
+      setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
     }, 10);
   }
 
   function handleStop() {
     clearInterval(timer.current);
+    dialog.current.open();
   }
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result='lost'/>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
